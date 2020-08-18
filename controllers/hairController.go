3d2416150e9b5eb7b80944b3ee174d64d909d540
eun-park <standingtherain15@gmail.com>
2020-08-18 16:39:27 +0900

    add getHairCycle function

---
+++ controllers/hairController.go
@@ -0,0 +1,35 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"mp_royal/configs"
+	"net/http"
+)
+
+type MonthlyHair struct {
+	HairReleaseID uint
+	Year uint `json:"year"`
+	Month uint `json:"month"`
+}
+
+type HairRelease struct {
+	ID              uint   `json:"hairId"`
+	Name            string `json:"hairName"`
+	ImgUrl          string `json:"hairImgUrl"`
+	MonthlyHairs []MonthlyHair
+}
+
+func GetHairCycle(c *gin.Context) {
+	var hairRelease HairRelease
+	var monthlyHaris []MonthlyHair
+	id := c.PostForm("id")
+
+	configs.DB.Where("id = ?", id).First(&hairRelease)
+	configs.DB.Where("hair_release_id = ?", id).Find(&monthlyHaris)
+	hairRelease.MonthlyHairs = monthlyHaris
+
+	//configs.DB.Model(&hairRelease).Association("monthly_hairs").Find(&hairRelease.MonthlyHairs)
+	c.JSON(http.StatusOK, gin.H{
+		"data": hairRelease,
+	})
+}
---
+++ routers/hairRouter.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"mp_royal/controllers"
+)
+
+func SetHairRouters(router *gin.RouterGroup) {
+	router.GET("/cycle", controllers.GetHairCycle)
+}
