c8ac24e6e2f978abd4afca31cf146297e0ec5a96
eun-park <standingtherain15@gmail.com>
2020-08-21 12:32:41 +0900

    add GetHairImg function

--- controllers/hairController.go
+++ controllers/hairController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"mp_royal/configs"
 	"net/http"
@@ -33,16 +34,11 @@ func GetHairCycle(c *gin.Context) {
 	})
 }
 
-type Hair struct {
+type Response struct {
 	ID uint `json:"hairId"`
 	Name string `json:"hairName"`
+	ImgUrl string `json:"hairImgUrl"`
 	Dates []string
-}
-
-type HairDate struct {
-	Year uint
-	Month uint
-	Hairs []Hair
 }
 
 type Request struct {
@@ -52,7 +48,7 @@ type Request struct {
 
 func GetHairDate(c *gin.Context) {
 	var request Request
-	var hairs []Hair
+	var responses []Response
 	c.ShouldBindJSON(&request)
 
 
@@ -60,13 +56,14 @@ func GetHairDate(c *gin.Context) {
 		Select("hair_releases.id, name").
 		Joins("join monthly_hairs on hair_releases.id = monthly_hairs.hair_release_id").
 		Where("monthly_hairs.year = ? and monthly_hairs.month = ?", request.Year, request.Month).
-		Find(&hairs)
+		Find(&responses)
 
-	for i, _ := range hairs {
+	for i, _ := range responses {
 		configs.DB.Table("monthly_hairs").
 			Select("concat(year, '-', month)").
-			Where("hair_release_id = ?", hairs[i].ID).
-			Scan(&hairs[i].Dates)
+			Where("hair_release_id = ?", responses[i].ID).
+			Scan(&responses[i].Dates)
+		responses[i].ImgUrl = GetHairImg(25017)
 	}
 
 /*
@@ -90,6 +87,19 @@ func GetHairDate(c *gin.Context) {
 */
 
 	c.JSON(http.StatusOK, gin.H{
-		"data": hairs,
+		"data": responses,
 	})
+}
+
+func GetHairImg(hairCode int) string {
+	return	fmt.Sprintf("https://maplestory.io/api/Character/" +
+		"{'itemId':2015,'version':'1105','region':'KMST'}," +
+		"{'itemId':12015,'version':'1105','region':'KMST'}," +
+		"{'itemId':30000,'version':'1105','region':'KMST'}," +
+		"{'itemId':%d,'version':'1105','region':'KMST'}," +
+		"{'itemId':1042162,'version':'1105','region':'KMST'}," +
+		"{'itemId':1060026,'version':'1105','region':'KMST'}" +
+		"/stand1/0?showears=false&showLefEars=false&showHighLefEars=undefined&resize=1&name=&flipX=false&bgColor=0,0,0,0",
+		hairCode,
+	)
 }
